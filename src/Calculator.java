39d7006c34a5e56c41003e620193ab4fc497afb8
MehmetAkin0307 <54609050+MehmetAkin0307@users.noreply.github.com>
2019-09-09 17:32:33 -0500

    REMOTE:Add divide method

--- src/Calculator.java
+++ src/Calculator.java
@@ -10,5 +10,6 @@ public class Calculator {
         double result = num1 - num2;
         return result;
     }
-
+public static double divide(double num1, double num2){
+     return num1 / num2;
 }
