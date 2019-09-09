f91454a0f55eed1d303a84df6f2f337f9e4a9382
Mehmet Akin <mehmetakin0307@gmail.com>
2019-09-09 10:10:19 -0500

    son duzeltme

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,4 +1,5 @@
 public class Calculator {
+
 //This is a very useful method.
     public static double add(double num1, double num2) {
 
@@ -9,14 +10,13 @@ public class Calculator {
         double result = num1 - num2;
         return result;
     }
-<<<<<<< HEAD
 
     public static double multiply(double num1, double num2) {
         return num1 * num2;
     }
 }
-=======
+
     public static double multiply(double num1,double num2){
         return num1*num2;
 }
->>>>>>> 0c398c13213b5dd681178ae6f6478524cf784ee4
+}
